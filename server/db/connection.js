036525aea0fd6d240d93d590c36ad0f73c89869a
irshad <coolirshadalam@gmail.com>
2023-07-26 00:27:08 +0530

    first commit

---
+++ server/db/connection.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+const url = process.env.DB_USERNAME;
+
+mongoose.connect(url, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+}).then(() => console.log('Connected to DB')).catch((e)=> console.log('Error', e))
